fde29431d0230d41e493fa035095c407a4b1203d
oubia <mohamed4oubia@gmail.com>
2021-05-10 21:16:21 +0000

    initial routes

--- gestion_app/main_app/urls.py
+++ gestion_app/main_app/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home,name="home"),
+    path('reception/', views.reception,name="reception"),
+    path('livraison/', views.livraison,name="livraison"),
+    path('historique/', views.historique,name="historique"),
 ]
--- gestion_app/main_app/views.py
+++ gestion_app/main_app/views.py
@@ -4,4 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'home.html')
+
+def reception(request):
+    return render(request,'reception.html')
+
+def livraison(request):
+    return render(request,'livraison.html')
+
+def historique(request):
+    return render(request,'historique.html')
