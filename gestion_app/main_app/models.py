1c84a687374822c30a5eb21c07660e374bb37a6a
oubia <mohamed4oubia@gmail.com>
2021-05-21 01:02:45 +0100

    cloning

--- gestion_app/main_app/models.py
+++ gestion_app/main_app/models.py
@@ -8,6 +8,8 @@ class Userconnection(models.Model):
 class Affectation(models.Model):
     Centre_titre = models.CharField(max_length=100)
 
+   
+
 class SousCentre(models.Model):
     centre_titre = models.ForeignKey(Affectation , on_delete=models.CASCADE)
     Sous_centre_titre = models.CharField(max_length=100)
@@ -17,9 +19,10 @@ class CategoriesModel(models.Model):
 
 class MaterielModel(models.Model):
     Numero_inventaire_entre = models.CharField(max_length = 10,blank=True,editable=False,unique=True,default=create_new_ref_number)
-    Designation_Object = models.CharField(max_length=100)
-    Category_name = models.ForeignKey(CategoriesModel,on_delete=models.DO_NOTHING)
+    Designation_Object = models.CharField(max_length=80)
+    Category_name = models.ForeignKey(CategoriesModel,on_delete=models.CASCADE)
     Quantite = models.IntegerField()
+    Etat = models.CharField(max_length=100,default='Nouveau')
     Emplacement = models.CharField(max_length=100)
     Date_reception = models.DateTimeField(auto_now_add=True) 
     Prix_achat_unite = models.FloatField()
@@ -39,36 +42,6 @@ class Livraison(models.Model):
     Prix_unitaire = models.FloatField()
     Decompte = models.CharField(max_length=100)
     Signatures = models.CharField(max_length=100)
-
-
-"""class historique(models.Model):
-    Livraison=
-    Centre_titre=
-    Materiel=
-    Prix_achat_unite = models.FloatField()
-    Prix_achat_total = models.FloatField()
-    Marque = models.CharField(max_length=100)
-    Model = models.CharField(max_length=100)
-    Serie = models.CharField(max_length=100)
-Observation = models.CharField(max_length=100)
-
-class model():
-name:primary 
-
-
-
-class produit():
-    model
-
-
-
-
-
-
-
-
-
-"""
 
 
 
