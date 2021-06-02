e681a38d3673ebfc6e659d57741cd0f629baf77c
ANASS19 <nassirri115@gmail.com>
2021-06-02 12:48:38 +0100

    disign

--- gestion_app/main_app/static/js/historique.js
+++ gestion_app/main_app/static/js/historique.js
@@ -49,13 +49,12 @@ function buildTable(data) {
         var row = `<tr>
 						<td>${data[i].Category_name}</td>
                         <td>${data[i].Designation_Object}</td>
-						<td>${data[i].Date_reception}</td>
+						<td>${data[i].Date_reception.slice(0, 10)}</td>
                         <td>${data[i].Emplacement}</td>
                         <td>${data[i].Etat}</td>
                         <td>${data[i].Numero_inventaire_entre}</td>
                         <td>${data[i].Quantite}</td>
                         <td>${data[i].Observation}</td>
-                        <td>${data[i].author_reception}</td>
                         <td>${data[i].Prix_achat_total}</td>
                         <td>${data[i].Prix_achat_unite}</td>
                         <td>${data[i].Serie}</td>
@@ -74,14 +73,13 @@ function buildTable1(data) {
         console.log('test')
         var row = `<tr>
                         <td>${data[i].Titre_livraison}</td>
-                        <td>${data[i].Date_sortie}</td>
+                        <td>${data[i].Date_sortie.slice(0, 10)}</td>
 						<td>${data[i].Centre}</td>
+                        <td>${data[i].Sous_centre_id}</td>
 						<td>${data[i].Decompte}</td>
                         <td>${data[i].Numero_inventaire_sortie}</td>
                         <td>${data[i].Prix_unitaire}</td>
-                        <td>${data[i].Numero_inventaire_entre}</td>
                         <td>${data[i].Quantite_livree}</td>
-                        <td>${data[i].Observation}</td>
                         <td>${data[i].author_livraison}</td>
 					  </tr>`
         table.innerHTML += row
