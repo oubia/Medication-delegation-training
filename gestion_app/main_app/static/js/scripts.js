61294a7f10326f66d512b8ac5dd531c6141ecb40
ANASS19 <nassirri115@gmail.com>
2021-06-03 19:06:34 +0100

    final design by anass

--- gestion_app/main_app/static/js/scripts.js
+++ gestion_app/main_app/static/js/scripts.js
@@ -107,3 +107,42 @@ function myFunction1() {
     }
 }
 myFunction1()
+
+
+
+function myFunction3() {
+    var input, filter, table, tr, td, td1,td2,td3, i, txtValue;
+    input = document.getElementById("myInput3");
+    console.log(input.innerHTML)
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable3");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[3];
+        td1 = tr[i].getElementsByTagName("td")[4];
+        td2 = tr[i].getElementsByTagName("td")[5];
+        td3 = tr[i].getElementsByTagName("td")[6];
+        if (td) {
+
+            txtValue = td.textContent || td.innerText;
+            txtValue1 = td1.textContent || td1.innerText;
+            txtValue2 = td2.textContent || td2.innerText;
+            txtValue3 = td3.textContent || td3.innerText;
+
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else if (txtValue1.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            }
+            else if (txtValue2.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            }
+            else if (txtValue3.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            }else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+myFunction3()
