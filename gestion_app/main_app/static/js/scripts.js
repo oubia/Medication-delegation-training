3ef6f79ecd4e89d9cccf250b72304659a05c8e5c
ANASS19 <nassirri115@gmail.com>
2021-06-04 00:00:41 +0100

    finich changes by anass

--- gestion_app/main_app/static/js/scripts.js
+++ gestion_app/main_app/static/js/scripts.js
@@ -31,7 +31,7 @@ setTimeout(function() {
 
 
 function myFunction() {
-    var input, filter, table, tr, td, td1, i, txtValue;
+    var input, filter, table, tr, td, td1,td2,td3, i, txtValue;
     input = document.getElementById("myInput");
     console.log(input.innerHTML)
     filter = input.value.toUpperCase();
@@ -40,15 +40,25 @@ function myFunction() {
     for (i = 0; i < tr.length; i++) {
         td = tr[i].getElementsByTagName("td")[0];
         td1 = tr[i].getElementsByTagName("td")[1];
+        td = tr[i].getElementsByTagName("td")[2];
+        td1 = tr[i].getElementsByTagName("td")[5];
         if (td) {
 
             txtValue = td.textContent || td.innerText;
             txtValue1 = td1.textContent || td1.innerText;
+            txtValue2 = td2.textContent || td2.innerText;
+            txtValue3 = td3.textContent || td3.innerText;
             if (txtValue.toUpperCase().indexOf(filter) > -1) {
                 tr[i].style.display = "";
             } else if (txtValue1.toUpperCase().indexOf(filter) > -1) {
                 tr[i].style.display = "";
-            } else {
+            }
+            else if (txtValue2.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            }
+            else if (txtValue3.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            }else {
                 tr[i].style.display = "none";
             }
         }
@@ -57,7 +67,7 @@ function myFunction() {
 myFunction()
 
 function myFunction2() {
-    var input, filter, table, tr, td, td1, i, txtValue;
+    var input, filter, table, tr, td, td1,td2,td3, i, txtValue;
     input = document.getElementById("myInput2");
     console.log(input.innerHTML)
     filter = input.value.toUpperCase();
@@ -66,47 +76,31 @@ function myFunction2() {
     for (i = 0; i < tr.length; i++) {
         td = tr[i].getElementsByTagName("td")[0];
         td1 = tr[i].getElementsByTagName("td")[1];
+        td2 = tr[i].getElementsByTagName("td")[2];
+        td3 = tr[i].getElementsByTagName("td")[3];
         if (td) {
 
             txtValue = td.textContent || td.innerText;
             txtValue1 = td1.textContent || td1.innerText;
+            txtValue2 = td2.textContent || td2.innerText;
+            txtValue3 = td3.textContent || td3.innerText;
             if (txtValue.toUpperCase().indexOf(filter) > -1) {
                 tr[i].style.display = "";
             } else if (txtValue1.toUpperCase().indexOf(filter) > -1) {
                 tr[i].style.display = "";
-            } else {
+            }
+            else if (txtValue2.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            }
+            else if (txtValue3.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            }else {
                 tr[i].style.display = "none";
             }
         }
     }
 }
 myFunction2()
-
-function myFunction1() {
-    var input, filter, table, tr, td, td1, i, txtValue;
-    input = document.getElementById("myInput1");
-    console.log(input.innerHTML)
-    filter = input.value.toUpperCase();
-    table = document.getElementById("myTable");
-    tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[0];
-        td1 = tr[i].getElementsByTagName("td")[1];
-        if (td) {
-
-            txtValue = td.textContent || td.innerText;
-            txtValue1 = td1.textContent || td1.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-            } else if (txtValue1.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-            } else {
-                tr[i].style.display = "none";
-            }
-        }
-    }
-}
-myFunction1()
 
 
 
